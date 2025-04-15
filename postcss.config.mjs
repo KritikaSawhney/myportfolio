3cfa6bff4885a9c2f45d41c42d25a455d244d16a
Kritika Sawhney <156064342+KritikaSawhney@users.noreply.github.com>
2025-04-15 15:22:26 +0530

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
+/** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    '@tailwindcss/postcss': {
-      config: './tailwind.config.ts', // Explicitly point to your config file
-    },
+    tailwindcss: {}, // Changed from '@tailwindcss/postcss'
     autoprefixer: {},
   },
 };
